5627dd093f5b8165cf893e6f4f261d10f7004e11
JoshuaABooker <JoshuaABooker@DESKTOP-R5ANG0N>
2017-08-31 09:27:16 -0400

    add tables & screens

---
+++ CBAAuth/CBAAuth.HTMLClient/Screens/Roles/ViewRole.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewRole.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Role.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Role." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
